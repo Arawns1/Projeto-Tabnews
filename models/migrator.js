660267a9d8277f1b1ab8ca7b801eb07b04b59e3e
Gabriel Damico <gabrieldamico22@gmail.com>
2025-12-15 21:34:04 -0300

    feat: create and implement `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,48 @@
+import database from "infra/database";
+import { resolve } from "node:path";
+import migrationRunner from "node-pg-migrate";
+
+const MIGRATIONS_PATH = resolve("infra", "migrations");
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: MIGRATIONS_PATH,
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
