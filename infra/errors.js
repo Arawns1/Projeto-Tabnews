3103dcc89425a28667634b8a44499b058f6b6187
Gabriel Damico <gabrieldamico22@gmail.com>
2026-01-17 14:15:50 -0300

    feat: add session creation endpoint and tests

--- infra/errors.js
+++ infra/errors.js
@@ -97,10 +97,31 @@ class NotFoundError extends Error {
   }
 }
 
+class UnauthorizedError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Usuário não autenticado", {
+      cause,
+    });
+    this.name = "UnauthorizedError";
+    this.action = action || "Faça novamente o login para continuar.";
+    this.statusCode = 401;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export {
   InternalServerError,
   MethodNotAllowedError,
   ServiceError,
   ValidationError,
   NotFoundError,
+  UnauthorizedError,
 };
